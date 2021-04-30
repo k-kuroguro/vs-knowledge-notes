2b96999c535da5aa3c0d04e02e72ce087ea5785e
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-05-01 01:13:16 +0900

    add shortcuts for tree view

--- src/noteExplorer.ts
+++ src/noteExplorer.ts
@@ -12,8 +12,12 @@ export class NoteExplorer {
 
    constructor(context: vscode.ExtensionContext) {
       this.fileSystemProvider = new FileSystemProvider();
-      this.watcherDisposer = this.fileSystemProvider.watch(vscode.Uri.file(Config.get(Config.Sections.notesDir)), { recursive: true, excludes: [] });
-      this.treeView = vscode.window.createTreeView(`${extensionName}.noteExplorer`, { treeDataProvider: this.fileSystemProvider });
+      this.treeView = vscode.window.createTreeView(`${extensionName}.noteExplorer`, { treeDataProvider: this.fileSystemProvider, showCollapseAll: true });
+      if (Config.get(Config.Sections.notesDir)) {
+         this.watcherDisposer = this.fileSystemProvider.watch(vscode.Uri.file(Config.get(Config.Sections.notesDir)), { recursive: true, excludes: [] });
+      } else {
+         this.watcherDisposer = { dispose: () => { } }
+      }
 
       context.subscriptions.push(
          this.treeView,
@@ -44,28 +48,27 @@ export class NoteExplorer {
          vscode.commands.registerCommand(`${extensionName}.noteExplorer.refresh`, () => this.refresh()),
          vscode.commands.registerCommand(`${extensionName}.noteExplorer.newFile`, (fileOrUndefined) => this.createNewFile(fileOrUndefined)),
          vscode.commands.registerCommand(`${extensionName}.noteExplorer.newFolder`, (fileOrUndefined) => this.createNewFolder(fileOrUndefined)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.findInFolder`, (file) => this.findInFolder(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.cut`, (file) => this.cut(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copy`, (file) => this.copy(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.paste`, (file) => this.paste(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copyPath`, (file) => this.copyPath(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copyRelativePath`, (file) => this.copyRelativePath(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.rename`, (file) => this.rename(file)),
-         vscode.commands.registerCommand(`${extensionName}.noteExplorer.delete`, (file) => this.delete(file))
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.findInFolder`, (fileOrUndefined) => this.findInFolder(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.cut`, (fileOrUndefined) => this.cut(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copy`, (fileOrUndefined) => this.copy(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.paste`, (fileOrUndefined) => this.paste(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copyPath`, (fileOrUndefined) => this.copyPath(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.copyRelativePath`, (fileOrUndefined) => this.copyRelativePath(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.rename`, (fileOrUndefined) => this.rename(fileOrUndefined)),
+         vscode.commands.registerCommand(`${extensionName}.noteExplorer.delete`, (fileOrUndefined) => this.delete(fileOrUndefined))
       );
    }
 
-   private openFile(uri: vscode.Uri): void {
+   private async openFile(uri: vscode.Uri): Promise<void> {
       if (!uri) return;
-      vscode.commands.executeCommand('vscode.open', uri);
+      await vscode.commands.executeCommand('vscode.open', uri);
+      await vscode.commands.executeCommand('workbench.action.focusSideBar');
    }
 
    private refresh(): void {
       this.fileSystemProvider.refresh();
    }
 
-   private createNewFile(): void;
-   private createNewFile(file: File): void;
    private createNewFile(file?: File): void {
       const selectedFile: File = file ? file : this.treeView.selection.length ? this.treeView.selection[0] : new File(vscode.Uri.file(Config.get(Config.Sections.notesDir)), vscode.FileType.Directory);
       const dirPath: vscode.Uri = selectedFile.type === vscode.FileType.Directory ? selectedFile.uri : vscode.Uri.file(path.dirname(selectedFile.uri.fsPath));
@@ -85,8 +88,6 @@ export class NoteExplorer {
       this.openFile(filePath);
    }
 
-   private createNewFolder(): void;
-   private createNewFolder(file: File): void;
    private createNewFolder(file?: File): void {
       const selectedFile: File = file ? file : this.treeView.selection.length ? this.treeView.selection[0] : new File(vscode.Uri.file(Config.get(Config.Sections.notesDir)), vscode.FileType.Directory);
       const dirPath: vscode.Uri = selectedFile.type === vscode.FileType.Directory ? selectedFile.uri : vscode.Uri.file(path.dirname(selectedFile.uri.fsPath));
@@ -104,23 +105,28 @@ export class NoteExplorer {
       this.fileSystemProvider.createDirectory(filePath);
    }
 
-   private findInFolder(file: File): void {
-      if (!file) return;
+   private findInFolder(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      if (selectedFile.type !== vscode.FileType.Directory) return;
       //TODO: add search web view
    }
 
-   private cut(file: File): void {
-      if (!file) return;
-      this.fileSystemProvider.setClipboard(file.uri, true);
+   private cut(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      this.fileSystemProvider.setClipboard(selectedFile.uri, true);
    }
 
-   private copy(file: File): void {
-      if (!file) return;
-      this.fileSystemProvider.setClipboard(file.uri, false);
+   private copy(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      this.fileSystemProvider.setClipboard(selectedFile.uri, false);
    }
 
-   private paste(file: File): void {
-      if (!file) return;
+   private paste(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
 
       const clipboard = this.fileSystemProvider.getClipboard();
       if (!clipboard) {
@@ -128,21 +134,27 @@ export class NoteExplorer {
          return;
       }
 
-      const destParent = file.type === vscode.FileType.Directory ? file.uri.fsPath : path.dirname(file.uri.fsPath);
+      const destParent = selectedFile.type === vscode.FileType.Directory ? selectedFile.uri.fsPath : path.dirname(selectedFile.uri.fsPath);
       const destBase = path.basename(clipboard.uri.fsPath);
       let count = 1;
       let filePath: vscode.Uri = vscode.Uri.joinPath(vscode.Uri.file(destParent), destBase);
       while (this.fileSystemProvider.exists(filePath)) {
          if (count == 1) {
-            filePath = vscode.Uri.joinPath(file.uri, `${destBase} copy`);
+            filePath = vscode.Uri.joinPath(vscode.Uri.file(destParent), `${destBase} copy`);
             count++;
          } else {
-            filePath = vscode.Uri.joinPath(file.uri, `${destBase} copy ${count++}`);
+            filePath = vscode.Uri.joinPath(vscode.Uri.file(destParent), `${destBase} copy ${count++}`);
          }
          if (count > 100) {
             vscode.window.showErrorMessage('File named "{filename} copy n" has reached copying limit. Please delete these files.');
             return;
          }
+      }
+
+      // check subdirectory
+      if (new RegExp(`^${clipboard.uri.fsPath.replace(/\\/g, '\\\\')}.*\\\\.*$`).test(filePath.fsPath)) {
+         vscode.window.showErrorMessage(`Cannot ${clipboard.cut ? 'cut' : 'copy'} "${clipboard.uri.fsPath}" to a subdirectory of itself, "${filePath.fsPath}".`);
+         return;
       }
 
       if (clipboard.cut) {
@@ -153,20 +165,23 @@ export class NoteExplorer {
       }
    }
 
-   private copyPath(file: File): void {
-      if (!file) return;
-      vscode.env.clipboard.writeText(file.uri.fsPath);
+   private copyPath(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      vscode.env.clipboard.writeText(selectedFile.uri.fsPath);
    }
 
-   private copyRelativePath(file: File): void {
-      if (!file) return;
-      vscode.env.clipboard.writeText(path.relative(Config.get(Config.Sections.notesDir), file.uri.fsPath));
+   private copyRelativePath(file?: File): void {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      vscode.env.clipboard.writeText(path.relative(Config.get(Config.Sections.notesDir), selectedFile.uri.fsPath));
    }
 
-   private async rename(file: File): Promise<void> {
-      if (!file) return;
+   private async rename(file?: File): Promise<void> {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
 
-      const dirname = vscode.Uri.file(path.dirname(file.uri.fsPath));
+      const dirname = vscode.Uri.file(path.dirname(selectedFile.uri.fsPath));
       const input = await vscode.window.showInputBox({
          prompt: 'Enter a name for the file.',
          validateInput: value => {
@@ -178,13 +193,14 @@ export class NoteExplorer {
       });
       if (!input) return;
 
-      this.fileSystemProvider.rename(file.uri, vscode.Uri.joinPath(dirname, input), { overwrite: false });
+      this.fileSystemProvider.rename(selectedFile.uri, vscode.Uri.joinPath(dirname, input), { overwrite: false });
    }
 
-   private async delete(file: File): Promise<void> {
-      if (!file) return;
-      if (await vscode.window.showWarningMessage(`Are you sure you want to delete "${path.basename(file.uri.fsPath)}"?`, 'Delete', 'Cancel') === 'Cancel') return;
-      this.fileSystemProvider.delete(file.uri, { recursive: true });
+   private async delete(file?: File): Promise<void> {
+      if (!file && !this.treeView.selection.length) return;
+      const selectedFile: File = file ? file : this.treeView.selection[0];
+      if (await vscode.window.showWarningMessage(`Are you sure you want to delete "${path.basename(selectedFile.uri.fsPath)}"?`, 'Delete', 'Cancel') === 'Cancel') return;
+      this.fileSystemProvider.delete(selectedFile.uri, { recursive: true });
    }
 
 }
