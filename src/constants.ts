7bbbc9d1e530b0e694e56d73eb368592be1c1179
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-05-04 13:49:11 +0900

    change publisher

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const extensionName: string = "simple-notes";
 export const extensionDisplayName: string = "Simple Notes";
-export const extensionPublisher: string = "k_kuroguro";
+export const extensionPublisher: string = "k-kuroguro";
